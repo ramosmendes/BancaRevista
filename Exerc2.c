205ec5927f44907b931836c3e5bc53fc534c4b56
Rivaldo Ramos <78941424+ramosmendes@users.noreply.github.com>
2023-10-17 15:10:51 -0300

    Update Exerc2.c

--- Exerc2.c
+++ Exerc2.c
@@ -2,25 +2,21 @@
 
 int main() {
     int linhas, colunas;
-
-    // Solicita ao usuário o tamanho em linhas e colunas da matriz
+    
     printf("Número de linhas: ");
     scanf("%d", &linhas);
 
     printf("Número de colunas: ");
     scanf("%d", &colunas);
 
-    // Verifica se as dimensões estão dentro do limite
     if (linhas > 1080 || colunas > 1920) {
         printf("Error! Valor excedido.\n");
         return 1;
     }
 
-    // Declara a matriz original e a matriz inversa
     int matriz_original[linhas][colunas];
     int matriz_inversa[linhas][colunas];
 
-    // Solicita ao usuário os valores da matriz original
     printf("Valores digitados para a matriz (por linha):\n");
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
@@ -33,7 +29,6 @@ int main() {
         }
     }
 
-    // Processa a matriz para gerar a matriz inversa
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
             matriz_inversa[i][j] = 255 - matriz_original[i][j];
@@ -49,7 +44,6 @@ int main() {
         printf("\n");
     }
 
-    // Exibe a matriz inversa
     printf("Matriz da imagem invertida (gerada pelo programa):\n");
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
